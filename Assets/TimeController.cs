76eaa5855e6da391d75ff8062af3b14bbc35fbde
garyn <gn0020@uah.edu>
2024-12-01 22:48:17 -0600

    made some static UI elements invisible when game is started

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -8,8 +8,13 @@ public class TimeController : MonoBehaviour
 
     Text timeText;
     float startingTime;
+    int timeLeft;
+    Text scoreText;
+    Text pointsEarnedText;
+    Text trickText;
+    Text boostText;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +27,31 @@ public class TimeController : MonoBehaviour
         Destroy(goImage, 4);
         Destroy(arrowImage, 4);
 
+        scoreText = GameObject.Find("ActualScoreText").GetComponent<Text>();
+        scoreText.text = "0";
+
+        pointsEarnedText = GameObject.Find("PointsEarnedText").GetComponent<Text>();
+        pointsEarnedText.text = "";
+
+        trickText = GameObject.Find("TrickTypeText").GetComponent<Text>();
+        trickText.text = "";
+
+        boostText = GameObject.Find("BoostText").GetComponent <Text>();
+        boostText.text = "";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float timePassed = Time.unscaledTime;
+        float temp = startingTime - timePassed;
+        timeLeft = (int)temp;
+
+        if (timeLeft >= 0)
+        {
+            timeText.text = timeLeft.ToString();
+        }
     }
 }
